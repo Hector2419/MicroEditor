aeb7b570b3d1f065991828cacefae0b26c6a7bfa
Alumno <Alumno@PC17>
2019-10-09 12:34:05 -0500

    asassadsadsadsadfas

---
+++ Mircronoseque/Mircronoseque/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mircronoseque
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+            int col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
+            lbCursorPosition.Text = "Linea " + (row + 1) + ", Letra " + (col + 1);
+            PbLleno.Value = col + 10;
+        }
+    }
+}
